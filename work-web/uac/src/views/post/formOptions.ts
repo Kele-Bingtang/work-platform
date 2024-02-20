791a7af9890cea4a571b159cc281f93e332fffb6
kele_Bingtang <2456019588@qq.com>
2024-02-20 23:31:38 +0800

    完成岗位管理的基础功能

---
+++ work-web/uac/src/views/post/formOptions.ts
@@ -0,0 +1,37 @@
+import type { FormOptionsProps } from "@work/components";
+import type { FormRules } from "element-plus";
+import { useLayoutStore } from "@/stores/layout";
+
+const rules = reactive<FormRules>({
+  postCode: [{ required: true, message: "请输入部门编码", trigger: "blur" }],
+  postName: [{ required: true, message: "请输入部门名称", trigger: "blur" }],
+});
+
+export const options: FormOptionsProps = {
+  form: {
+    inline: true,
+    labelPosition: "right",
+    labelWidth: "80px",
+    size: "default",
+    fixWidth: true,
+    rules: rules,
+  },
+  columns: [
+    {
+      formItem: { label: "岗位编码", prop: "postCode" },
+      attrs: { el: "el-input", props: { clearable: true, placeholder: "请输入 角色编码" } },
+    },
+    {
+      formItem: { label: "岗位名称", prop: "postName" },
+      attrs: { el: "el-input", props: { clearable: true, placeholder: "请输入 领导" } },
+    },
+    {
+      formItem: { label: "显示顺序", prop: "orderNum" },
+      attrs: { el: "el-input-number", defaultValue: 0 },
+    },
+    {
+      formItem: { label: "介绍", prop: "intro", br: true },
+      attrs: { el: "el-input", props: { type: "textarea", clearable: true, placeholder: "请输入 介绍" } },
+    },
+  ],
+};
---
+++ work-web/uac/src/views/post/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="post-container">
+    <ProTable
+      ref="proTable"
+      :request-api="list"
+      :columns="columns"
+      :search-col="{ xs: 1, sm: 1, md: 2, lg: 3, xl: 3 }"
+      style="height: 90%"
+      :detailForm="detailForm"
+      :border="false"
+    ></ProTable>
+  </div>
+</template>
+
+<script setup lang="tsx" name="Post">
+import { ProTable, message } from "work";
+import { list, addOne, editOne, deleteOne, type Post } from "@/api/post";
+import { findItemNested, type DialogForm, type TableColumnProps } from "@work/components";
+import { options } from "./formOptions";
+import { useLayoutStore } from "@/stores/layout";
+import { ElMessageBox } from "element-plus";
+
+const columns: TableColumnProps<Post.PostInfo[]>[] = [
+  { prop: "postCode", label: "部门编码", search: { el: "el-input" } },
+  { prop: "postName", label: "部门名称", search: { el: "el-input" } },
+  { prop: "orderNum", label: "排序" },
+  {
+    prop: "status",
+    label: "状态",
+    fieldNames: { value: "dictValue", label: "dictLabel" },
+    enum: () => useLayoutStore().getDictData("sys_normal_status"),
+    search: { el: "el-select" },
+    render: ({ row }) => {
+      return (
+        <>
+          {row.status !== undefined && (
+            <el-switch
+              vModel={row.status}
+              activeValue={1}
+              inactiveValue={0}
+              activeText="启用"
+              inactiveText="停用"
+              inlinePrompt
+              onChange={(value: number) => statusChange(value, row)}
+            />
+          )}
+        </>
+      );
+    },
+  },
+  { prop: "createTime", label: "创建时间" },
+  { prop: "operation", label: "操作" },
+];
+
+const detailForm: DialogForm = {
+  options: options,
+  addApi: addOne,
+  editApi: editOne,
+  deleteApi: deleteOne,
+  dialog: {
+    title: (_, status) => (status === "add" ? "新增" : "编辑"),
+    width: "45%",
+    top: "5vh",
+    closeOnClickModal: false,
+  },
+};
+
+const statusChange = async (value: number, row: Post.PostInfo) => {
+  const statusEnum = await useLayoutStore().getDictData("sys_normal_status");
+  const filterData = findItemNested(statusEnum.data, value + "", "dictValue", "");
+
+  if (!filterData?.dictLabel) return (row.status = 1) && message.warning("不存在状态");
+
+  ElMessageBox.confirm(
+    `确认要 <span style="color: teal">${filterData?.dictLabel}</span> 【${row.deptName}】部门吗`,
+    "系统提示",
+    {
+      dangerouslyUseHTMLString: true,
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "warning",
+    }
+  )
+    .then(() => {
+      editOne({ id: row.id, deptId: row.deptId, parentId: row.parentId, status: value })
+        .then(res => {
+          if (res.status === "success") message.success("修改成功");
+          else {
+            statusRecover(value, row);
+          }
+        })
+        .catch(() => statusRecover(value, row));
+    })
+    .catch(() => statusRecover(value, row));
+};
+
+const statusRecover = (value: number, row: Post.PostInfo) => {
+  if (value === 0) return (row.status = 1);
+  if (value === 1) return (row.status = 0);
+};
+</script>
+
+<style lang="scss" scoped>
+.post-container {
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+}
+</style>
